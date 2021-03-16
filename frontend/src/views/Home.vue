1cc9f705fe7891b5cdf446bec727b018d19e6eeb
Anael Damette <damette.anael@gmail.com>
2021-03-16 11:38:55 +0100

    finition visuel

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="home">
+  <div class="home container ">
     <HelloWorld msg="Bienvenue chez Groupomania" />
+    <div v-if="User.token != null" class='jumbotron shadow-box m-3 p-2'>
+    <MesMessages /></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import MesMessages from '../components/MesMessages'
+import { mapState } from "vuex";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    MesMessages
+  },
+  computed: {
+    ...mapState(["User"]),
+  },
 }
 </script>
