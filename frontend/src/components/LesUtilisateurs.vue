04569f8305036345d107849fbf66a9fd99dbf04d
Anael Damette <damette.anael@gmail.com>
2021-03-22 17:18:06 +0100

    Mise en place des post finition, suppression users, suppresion post, modification post

---
+++ frontend/src/components/LesUtilisateurs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello container jumbotron shadow-box ml-auto mr-auto mt-5">
+      <div class=" bg-light d-flex justify-content-between shadow-box"> 
+        <div class="d-flex ml-1 pl-1 align-items-center justify-content-between"> 
+            <p class='m-2'> le nom d'utilisateur : <strong>{{ users.username}}</strong> </p>
+      </div>
+      <button
+            @click="deleteUser"
+            class="btn btn-primary m-2"
+          >
+            <i class="fas fa-ban"></i>
+          </button>
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+  
+export default {
+  name: "LesUtilisateurs",
+  props: {
+    users: {
+      type: Object,
+      username: String,
+    }
+  },
+  computed: {
+    ...mapState(["User"])
+  },
+  methods: {
+      deleteUser() {
+          console.log("Je suis bien en train de delete un User")
+      }
+  }
+};
+</script>
---
+++ frontend/src/views/Admin.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home container">
+    <HelloWorld msg="Administration de Groupomania" />
+    <LesUtilisateurs v-for="users in allUsers" :users="users" v-bind:key="users.uuid" />
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import LesUtilisateurs from "@/components/LesUtilisateurs.vue";
+import { mapState } from "vuex";
+import axios from "axios";
+
+export default {
+  name: "Admin",
+  components: {
+    HelloWorld,
+    LesUtilisateurs
+  },
+  data() {
+    return {
+      users: {
+        username: "",
+        uuid: "",
+      },
+      allUsers: []
+    }
+  },
+  computed: {
+    ...mapState(["User"]),
+  },
+  methods: {
+  getUser(){
+
+    console.log("Fonction getUser")
+    let uuid = localStorage.getItem('uuid')
+
+
+    axios
+
+    .get("http://localhost:3000/api/auth/admin/" + uuid)
+    .then((response) => {
+      this.allUsers = response.data
+      console.log(this.allUsers)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  },
+  },
+  mounted(){
+    this.getUser()
+  }
+}
+
+</script>
