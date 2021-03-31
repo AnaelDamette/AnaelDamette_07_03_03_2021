4f0e1317c6ad1d814b895428708d218026aef094
Anael Damette <damette.anael@gmail.com>
2021-03-31 22:30:38 +0200

    changement Mysql

--- MysqlDataBase/groupotwit.sql
+++ MysqlDataBase/groupotwit.sql
@@ -16,6 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `signal` tinyint(1) DEFAULT NULL,
+  `uuidComment` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `message` text NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `userId` int DEFAULT NULL,
+  `postId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  KEY `postId` (`postId`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `posts`
 --
 
@@ -47,6 +80,62 @@ LOCK TABLES `posts` WRITE;
 INSERT INTO `posts` VALUES (18,NULL,'ea6d8e80-d69b-484d-b7d0-2a516a661296','la Horde du Contrevent','La Horde du Contrevent est un roman mêlant science-fiction1 et fantasy2,3 écrit par Alain Damasio et publié aux éditions La Volte en 2004. Dans sa première édition, le livre était accompagné d\'un CD qui en constituait la « bande originale ». Le roman a été un succès commercial et critique, et a remporté le Grand prix de l\'Imaginaire en 2006.\n\nIls sont vingt-trois, forment la trente-quatrième Horde du Contrevent et ont entre vingt-sept et quarante-trois ans. Dans un monde balayé par les vents, ils ont été formés depuis l\'enfance dans un seul but : parcourir le monde, d\'ouest en est, de l\'Aval vers l\'Amont, à contre-courant face au vent, à travers la plaine, l\'eau et les pics glacés, pour atteindre le mythique Extrême-Amont, la source de tous les vents.\n\nTous différents mais tous unis, ils forment une horde autonome et solidaire, qui avance dans un seul objectif, luttant constamment contre le vent. Profitant du savoir et de l\'expérience de huit siècles d\'échecs, on la dit la meilleure et l\'ultime Horde, celle qui atteindra enfin l’Extrême-Amont.','http://localhost:3000/images/8.jpg1616769260882.jpg','2021-03-26 14:34:01','2021-03-26 14:34:20',8);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `sequelizemeta`
+--
+
+DROP TABLE IF EXISTS `sequelizemeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sequelizemeta` (
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sequelizemeta`
+--
+
+LOCK TABLES `sequelizemeta` WRITE;
+/*!40000 ALTER TABLE `sequelizemeta` DISABLE KEYS */;
+INSERT INTO `sequelizemeta` VALUES ('20210310140524-create-user.js');
+/*!40000 ALTER TABLE `sequelizemeta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (8,'afa3ef2a-e22d-4d36-8cad-284ddd582c44','admin','1ebb4c922f92f99755ab1687f22dc95cfb33cb33','$2b$10$XzCnUCGpZ.B2bskNpyWQ..De9b0YxrjTQKnW9WuxybLN4Izv2HNZC',1,'2021-03-25 07:20:42','2021-03-25 07:20:42'),(16,'4f896e9a-14e1-40f0-b214-e076984c6cbc','mail','12be489701b5f38453b2488eed','$2b$10$cRZ.ne0L18aDRJupqmJdtOOFN27ZJgPPfVO5kY77KLQhshYjahrO2',0,'2021-03-30 12:30:19','2021-03-30 12:30:19');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -57,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-31 21:17:44
+-- Dump completed on 2021-03-31 22:27:59
